1f2f9ef3dab96e4a4c674bfd52c1b09eade71b39
shuchirinamdar <inamdar.shuchir77@gmail.com>
2017-01-15 01:07:19 -0700

    Adding controller functions

--- kwik/www/js/controllers.js
+++ kwik/www/js/controllers.js
@@ -1,9 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AnalyseCtrl', function($scope) {
-
-
-})
+.controller('AnalyseCtrl', function($scope) {})
 
 .controller('SyncCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
@@ -25,9 +22,72 @@ angular.module('starter.controllers', [])
 //})
 
 .controller('TransactCtrl', function($scope) {
-  $scope.settings = {
-    isIncome: true
-  };
+  $scope.settings = {isIncome: true};
+  
+	$scope.user = {};
+	var userId="Ajay01";
+	var dict = new buckets.Dictionary();
 
-   $scope.productList = ["Abc", "xyz", "cef"];
+	$scope.productList = ["Abc", "xyz", "cef"];
+	
+	firebase.database().ref().child('kwikUsers').orderByChild('uid').equalTo(userId).on("value", function(snapshot) {
+		snapshot.forEach(function(data) {
+			data.child('userDataSet').val().forEach(function(childData){
+				var a = childData;
+				var newKey = a.userData.hitCount+"_"+a.userData.userTag;
+				if(dict.containsKey(newKey))
+				{
+					var l_userData = dict.get(newKey);
+					l_userData.hitCount +=1;
+					dict.set(l_userData);
+				} else {
+					dict.set(newKey, a.userData);
+				}
+			});
+		});
+		
+		var keys = dict.keys();
+		keys.sort();
+		
+		for(var i=keys.length-1; i>=0; i--){
+			console.log(dict.get([keys[i]]));
+		}
+	});
+	
+	
+	$scope.push = function()
+	{
+		userId="Ajay01";
+		var hitCount = 0;
+		var userTag = $scope.user.tag;
+
+		
+		//var kwikRef = firebase.database().ref('kwikUsers/');
+		firebase.database().ref().child('kwikUsers').orderByChild('uid').equalTo(userId).on("value", function(snapshot) {
+			snapshot.forEach(function(data) {
+				data.child('userDataSet').val().forEach(function(childData){
+				var a = childData;
+				if(a.userData.userTag == userTag)
+					{
+						hitCount = a.userData.hitCount;
+						hitCount += 1;
+						a.userData.hitCount += hitCount;
+					}
+				});
+			});
+			
+			if(hitCount == 0)
+			{
+				pushData(hitCount);
+			}
+		});
+	}
+
+	function pushData(hitCount)
+	{
+		alert("I am in hitCount == 0");
+		firebase.database().ref().child('kwikUsers').push({uid: userId, userDataSet: [{userData:{userTag:$scope.user.tag, productPrice:$scope.user.price, quantity:$scope.user.quantity, hitCount : 1}}]});
+		console.log('posted message to firebase');		
+	}
 });
+
